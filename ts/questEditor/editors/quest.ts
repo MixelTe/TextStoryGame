b8b4105da400f5973e5207bedcbe6f0c276a122d
Mike <63655075+MixelTe@users.noreply.github.com>
2021-07-23 19:35:10 +0300

    questEditor-quest

--- ts/questEditor/editors/quest.ts
+++ ts/questEditor/editors/quest.ts
@@ -1,5 +1,6 @@
-import { Div } from "../../functions.js";
+import { Div, Input, TextArea } from "../../functions.js";
 import { Quest } from "../../questStructure.js";
+import { InputPlus, TextAreaPlus } from "../functions.js";
 
 export class Editor_Quest
 {
@@ -7,6 +8,19 @@ export class Editor_Quest
 	public render(body: HTMLElement)
 	{
 		body.innerHTML = "";
-		body.appendChild(Div([], []));
+		body.appendChild(Div([], [
+			Div("pg2-line", [
+				Div([], [], "Название:"),
+				InputPlus("pg2", "text", "Название квеста")(
+					inp => { this.quest.name = inp.value; this.save(); },
+					inp => inp.value = this.quest.name)(),
+			]),
+			Div("pg2-line", [
+				Div([], [], "Описание:"),
+				TextAreaPlus("Описание квеста", "pg2")(
+					ta => { this.quest.description = ta.value; this.save(); },
+					ta => ta.value = this.quest.description)(),
+			]),
+		]));
 	}
 }
