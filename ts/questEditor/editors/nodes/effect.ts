744d5ea0ded5ac0dcbcc327074759f24b7f588e7
Mike <63655075+MixelTe@users.noreply.github.com>
2021-08-05 12:31:33 +0300

    questEditor-chapter-effect

--- ts/questEditor/editors/nodes/effect.ts
+++ ts/questEditor/editors/nodes/effect.ts
@@ -1,5 +1,6 @@
+import { Div, Option, Select, Span } from "../../../functions.js";
 import { ChapterContent_effect } from "../../../questStructure.js";
-import { QuestFull } from "../../functions.js";
+import { InputPlus, QuestFull } from "../../functions.js";
 
 export class Editor_Node_effect
 {
@@ -16,13 +17,37 @@ export class Editor_Node_effect
 		const node = this.quest.chapters.chapters[this.indexes[0]][this.indexes[1]].content[this.indexes[2]];
 		if (node.type != "effect")
 			throw new Error('Editor_Node_effect: node.type != "effect"');
+		body.appendChild(Div("pg2-block-small", [
+			Div("pg2-line-small", [
+				Span("margin-right", [], "Эффект:"),
+				Select([], [
+					Option("Затемнение экрана", "darkScreen"),
+					Option("Засветление экрана", "whiteScreen"),
+					Option("Тряска экрана", "shake"),
+				],
+					select => { node.effectName = <any>select.value; this.save(); },
+					select => { select.value = node.effectName; },
+				)
+			]),
+			Div("pg2-line-small", [
+				Span("margin-right", [], "Длительность:"),
+				InputPlus([], "number", "Длительность")(
+					inp =>
+					{
+						if (isNaN(inp.valueAsNumber)) inp.valueAsNumber = 250;
+						node.duraction = inp.valueAsNumber;
+						this.save();
+					},
+					inp => { inp.valueAsNumber = node.duraction; })(),
+			]),
+		]));
 	}
 	private createNode()
 	{
 		const node = <ChapterContent_effect>{
 			type: "effect",
 			effectName: "darkScreen",
-			duraction: 1,
+			duraction: 250,
 		};
 		return node;
 	}
