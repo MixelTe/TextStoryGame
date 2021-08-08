ca7487489639818f6a8aa0d335df4e78e8a1b342
Mike <63655075+MixelTe@users.noreply.github.com>
2021-08-08 13:51:02 +0300

    some changes

--- ts/questEditor/editors/nodeTools.ts
+++ ts/questEditor/editors/nodeTools.ts
@@ -19,28 +19,24 @@ export function createNodeTypeSelect()
 }
 export function createNode(type: string)
 {
-	let node: ChapterPartNode;
 	switch (type) {
-		case "speech": node = Editor_Node_speech.createNode(); break;
-		case "question": node = Editor_Node_question.createNode(); break;
-		case "effect": node = Editor_Node_effect.createNode(); break;
-		case "change": node = Editor_Node_change.createNode(); break;
-		case "splitter": node = Editor_Node_splitter.createNode(); break;
+		case "speech": return Editor_Node_speech.createNode();
+		case "question": return Editor_Node_question.createNode();
+		case "effect": return Editor_Node_effect.createNode();
+		case "change": return Editor_Node_change.createNode();
+		case "splitter": return Editor_Node_splitter.createNode();
 		default: throw new Error(`Unexpected node type: ${type}`);
 	}
-	return node;
 }
 export function renderNode(node: ChapterPartNode, quest: QuestFull, save: () => void, collapsed: boolean)
 {
 	const type = node.type;
-	let nodeEl: HTMLDivElement;
 	switch (node.type) {
-		case "speech": nodeEl = new Editor_Node_speech(quest, node, save).render(collapsed); break;
-		case "question": nodeEl = new Editor_Node_question(quest, node, save).render(collapsed); break;
-		case "effect": nodeEl = new Editor_Node_effect(quest, node, save).render(collapsed); break;
-		case "change": nodeEl = new Editor_Node_change(quest, node, save).render(collapsed); break;
-		case "splitter": nodeEl = new Editor_Node_splitter(quest, node, save).render(collapsed); break;
+		case "speech": return new Editor_Node_speech(quest, node, save).render(collapsed);
+		case "question": return new Editor_Node_question(quest, node, save).render(collapsed);
+		case "effect": return new Editor_Node_effect(quest, node, save).render(collapsed);
+		case "change": return new Editor_Node_change(quest, node, save).render(collapsed);
+		case "splitter": return new Editor_Node_splitter(quest, node, save).render(collapsed);
 		default: throw new Error(`Unexpected node type: ${type}`);
 	}
-	return nodeEl;
 }
